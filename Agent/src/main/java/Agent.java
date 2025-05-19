f58ab367f019d9d0c196ae7f401a1244b1444299
IT24100087 <IT24100087@my.sliit.lk>
2025-05-19 23:20:33 +0530

    Add Agent class

---
+++ Agent/src/main/java/Agent.java
@@ -0,0 +1,69 @@
+
+public class Agent {
+    private String username;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+    private String licenseNumber;
+    private String agencyName;
+    private String areasServed;
+    private String bio;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+    public String getAgencyName() {
+        return agencyName;
+    }
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+    public String getAreasServed() {
+        return areasServed;
+    }
+    public void setAreasServed(String areasServed) {
+        this.areasServed = areasServed;
+    }
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+
+}
